69244e680f62504a355063ec911aedc6636ac208
[JInsunnya] <[jsk5754@naver.com]>
2024-11-16 16:02:25 +0900

    Initialize project using Create React App

---
+++ src/assets/font.jsx
@@ -0,0 +1,7 @@
+// 넥슨 메이플스토리
+@font-face {
+    font-family: 'MaplestoryOTFBold';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/MaplestoryOTFBold.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
