4cc3284253b7aac12422246a4c41f937e0d424b4
sooyeon <limsoo0816@naver.com>
2024-11-19 00:28:36 +0900

    main,info merge

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Landing from './pages/Landing';
-import GlobalStyle from './pages/GlobalStyles';
-import './App.css';
-import Introduction from './pages/Introduction';
-import Introduction2 from './pages/Introduction2';
-import Introduction3 from './pages/Introduction3';
-import IntroFooter from './components/IntroductionFooter';
-import Header1 from './components/Header1';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import GlobalStyle from "./pages/GlobalStyles";
+import "./App.css";
+import Introduction from "./pages/Introduction";
+import Introduction2 from "./pages/Introduction2";
+import Introduction3 from "./pages/Introduction3";
+import IntroFooter from "./components/IntroductionFooter";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Header1 from "./components/Header1";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<Introduction />} />
+        <Route path="/introduction" element={<Introduction />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as L from "../styles/LandingStyles";
+import LogoImg from "../assets/logofinal.svg";
+import LoginBtn from "../assets/loginbtn.svg";
+import Globe from "../assets/globe.svg";
+import Ministarpink from "../assets/ministarpink.png";
+import Ministaryellow from "../assets/ministaryellow.png";
+import Ministarneon from "../assets/ministary_green.png";
+import Ministarred from "../assets/ministarred.png";
+import Ministargreen from "../assets/ministargreen.png";
+import Ministarblue from "../assets/ministarblue.png";
+import UserId from "../assets/mail.png";
+import UserPassword from "../assets/password.png";
+const Login = () => {
+  const [isLoginClicked, setIsLoginClicked] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    setIsLoginClicked(true);
+  };
+  const handleSignUpClick = () => {
+    navigate("/signup");
+  };
+  return (
+    <L.Container>
+      <L.Logo>
+        <img id="LogoImg" src={LogoImg} />
+      </L.Logo>
+      <L.LoginForm>
+        <div className="userlogin">
+          <img id="userId" src={UserId} />
+          <input type="text" placeholder="아이디" />
+        </div>
+        <div className="userlogin">
+          <img id="userPassword" src={UserPassword} />
+          <input type="password" placeholder="비밀번호" />
+        </div>
+      </L.LoginForm>
+      <L.LoginBtn onClick={handleLoginClick}>
+        <img id="LoginBtn" src={LoginBtn} />
+      </L.LoginBtn>
+      <L.IsItFirst>
+        <p>
+          밍글이 처음이신가요? <a onClick={handleSignUpClick}>회원가입</a>
+        </p>
+      </L.IsItFirst>
+
+      <L.LandingImg style={{ bottom: "-150px" }}>
+        <L.Stars>
+          <img id="Ministarpink" src={Ministarpink} />
+          <img id="Ministaryellow" src={Ministaryellow} />
+          <img id="Ministarneon" src={Ministarneon} />
+          <img id="Ministarred" src={Ministarred} />
+          <img id="Ministargreen" src={Ministargreen} />
+          <img id="Ministarblue" src={Ministarblue} />
+        </L.Stars>
+        <img src={Globe} />
+      </L.LandingImg>
+    </L.Container>
+  );
+};
+export default Login;
